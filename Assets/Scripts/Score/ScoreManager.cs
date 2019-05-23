5d7c83598a910b4a5b9dac6cbaa34b9ca69e4d76
Khalil Yamada Gangeme <khalil.yamada@hotmail.com>
2019-05-23 00:41:07 -0300

    Adicionado um script que cuida dos canhões  -> Ativa  -> Melhora
    Torre agora evolui e começa em estado básico

--- Assets/Scripts/Score/ScoreManager.cs
+++ Assets/Scripts/Score/ScoreManager.cs
@@ -9,62 +9,9 @@ public class ScoreManager : MonoBehaviour
 
     public float score = 0;
 
-    private bool canhao_01_vivo;
-    private bool canhao_02_vivo;
-    private bool canhao_03_vivo;
-    private bool canhao_04_vivo;
-
-    public GameObject canhao_01;
-    public GameObject canhao_02;
-    public GameObject canhao_03;
-    public GameObject canhao_04;
-
-    public Turrent turretScript_canhao_01;
-    public Turrent turretScript_canhao_02;
-    public Turrent turretScript_canhao_03;
-    public Turrent turretScript_canhao_04;
-
-
-
-    private void Start()
-    {
-        turretScript_canhao_01 = canhao_01.GetComponent<Turrent>();
-        turretScript_canhao_02 = canhao_02.GetComponent<Turrent>();
-        turretScript_canhao_03 = canhao_03.GetComponent<Turrent>();
-        turretScript_canhao_04 = canhao_04.GetComponent<Turrent>();
-    }
-
-
     void Update()
     {
-
-
-
-        Canhao(KeyCode.Alpha1, canhao_01, canhao_01_vivo, turretScript_canhao_01);
-        Canhao(KeyCode.Alpha2, canhao_02, canhao_02_vivo, turretScript_canhao_02);
-        Canhao(KeyCode.Alpha3, canhao_03, canhao_03_vivo, turretScript_canhao_03);
-        Canhao(KeyCode.Alpha4, canhao_04, canhao_04_vivo, turretScript_canhao_04);
-
-
-
         AtualizaPontuacao();
-    }
-
-
-    void Canhao(KeyCode tecla, GameObject qualCanhao, bool canhaoVivo, Turrent qualScript)
-    {
-        if (Input.GetKeyDown(tecla) && score >= 300 && canhaoVivo == true)
-        {
-            score -= 300;
-            qualScript.FireRate += 0.1f;
-        }
-
-        if (Input.GetKeyDown(tecla) && score >= 200 && canhaoVivo == false)
-        {
-            score -= 200;
-            qualCanhao.SetActive(true);
-            canhaoVivo = true;
-        }
     }
 
 
---
+++ Assets/Scripts/Structure/CanonManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonManager : MonoBehaviour
+{
+    public ScoreManager pontuacao;
+    private bool canhaoVivo;
+
+    [Header("Canhões")]
+    public GameObject canhao;
+    public Turrent turretScript;
+
+    [Header("Qual tecla")]
+    [SerializeField]    
+    private KeyCode tecla;
+
+
+
+    private void Start()
+    {
+        pontuacao = GameObject.FindWithTag("Score").GetComponent<ScoreManager>();
+    }
+
+
+    private void Update()
+    {
+        Canhao();
+    }
+
+
+    void Canhao()
+    {
+        if (Input.GetKeyDown(tecla) && pontuacao.score >= 300 && canhaoVivo == true && turretScript.FireRate <= 1f)
+        {
+            pontuacao.score -= 300;
+            turretScript.FireRate += 0.1f;
+        }
+
+        if (Input.GetKeyDown(tecla) && pontuacao.score >= 200 && canhaoVivo == false)
+        { 
+            pontuacao.score -= 200;
+            canhao.SetActive(true);
+            canhaoVivo = true;
+        }
+    }    
+}
