3de8e667b60b08405fdbca912efb8391e04bedf1
Khalil Yamada Gangeme <khalil.yamada@hotmail.com>
2019-05-29 03:00:59 -0300

    Adicionada a interface de compra dos canhões
    Adicionado os ícones de canhão e torre 
    Junto com os leveis atuais das estruturas e seus respectivos valores de compra
    SOMENTE CANHÃO ESTÁ FUNCIONAL !!!

--- Assets/Scripts/Structure/CanonManager.cs
+++ Assets/Scripts/Structure/CanonManager.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CanonManager : MonoBehaviour
 {
     public ScoreManager pontuacao;
     private bool canhaoVivo;
+
+    public TextMeshProUGUI textLevel;
+    public TextMeshProUGUI textLevelUp;
+
 
     [Header("Canhões")]
     public GameObject canhao;
@@ -15,7 +20,11 @@ public class CanonManager : MonoBehaviour
     [SerializeField]    
     private KeyCode tecla;
 
-
+    [Header("Preços")]
+    [SerializeField]
+    private float criar = 300;
+    [SerializeField]
+    private float upar = 200;
 
     private void Start()
     {
@@ -26,22 +35,44 @@ public class CanonManager : MonoBehaviour
     private void Update()
     {
         Canhao();
+        LevelAtual();
     }
 
 
     void Canhao()
     {
-        if (Input.GetKeyDown(tecla) && pontuacao.score >= 300 && canhaoVivo == true && turretScript.FireRate <= 1f)
+        if (Input.GetKeyDown(tecla) && pontuacao.score >= upar && canhaoVivo == true && turretScript.FireRate <= 1f)
         {
-            pontuacao.score -= 300;
+            pontuacao.score -= upar;
             turretScript.FireRate += 0.1f;
+            upar = upar + 30;
         }
 
-        if (Input.GetKeyDown(tecla) && pontuacao.score >= 200 && canhaoVivo == false)
+        if (Input.GetKeyDown(tecla) && pontuacao.score >= criar && canhaoVivo == false)
         { 
-            pontuacao.score -= 200;
+            pontuacao.score -= criar;
             canhao.SetActive(true);
             canhaoVivo = true;
         }
     }    
+
+    void LevelAtual()
+    {
+        if (turretScript.FireRate <= 0.9f && canhaoVivo == false)
+        {
+            textLevelUp.text = "Next Lv." + Mathf.RoundToInt(criar);
+            textLevel.text = "Lv.0";
+        }
+        else if (turretScript.FireRate <= 0.99f)
+        {
+            textLevelUp.text = "Next Lv." + Mathf.RoundToInt(upar);
+            textLevel.text = "Lv." + Mathf.RoundToInt(turretScript.FireRate * 10);
+        }
+        else
+        {
+            textLevelUp.text = "Next Lv.Max";
+            textLevel.text = "Lv.Max";
+        }
+
+    }
 }
